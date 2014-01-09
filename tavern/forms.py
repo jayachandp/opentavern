38cd1bdcb67b96146bd7766df1e3de49c3fb5f63
krvc <krvc.it@gmail.com>
2014-01-09 17:34:21 +0530

    Replaced exclude with fields

--- tavern/forms.py
+++ tavern/forms.py
@@ -1,6 +1,6 @@
 """ Opentavern Forms """
 
-from django.forms import ModelForm
+from django.forms import ModelForm, DateField
 from .models import TavernGroup, Event
 
 
@@ -8,11 +8,16 @@ class CreateGroupForm(ModelForm):
     """ CreateGroupForm """
     class Meta:
         model = TavernGroup
-        exclude = ['creator', 'organizers']
+        fields = ['name', 'group_type', 'description',
+                  'members_name', 'country', 'city']
 
 
 class CreateEventForm(ModelForm):
     """ CreateEventForm """
+    starts_at = DateField(input_formats=['%d/%m/%Y', '%d-%m-%Y'])
+    end_at = DateField(input_formats=['%d/%m/%Y', '%d-%m-%Y'])
+
     class Meta:
         model = Event
-        exclude = ['creator']
+        fields = ['group', 'name', 'description', 'starts_at',
+                  'end_at', 'location']
