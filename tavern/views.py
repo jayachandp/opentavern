34783a40e44aa5b6a4c8779d779a3e515cf34946
krvc <krvc.it@gmail.com>
2014-01-09 17:35:39 +0530

    Fixed redirect url

--- tavern/views.py
+++ tavern/views.py
@@ -132,7 +132,7 @@ def create_event(request, template='create_event.html'):
             event = form.save(commit=False)
             event.creator = request.user
             event.save()
-            return redirect("group_details")
+            return redirect("/events/%d" % event.id)
 
     context = {'form': form}
     return render(request, template, context)
